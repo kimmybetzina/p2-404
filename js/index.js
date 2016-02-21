c4bbaaf5d20f687acc836dd4a05eb86dcd48aec2
kimmybetzina <KimmyBetzina@Kimberlys-iMac.local>
2016-02-21 12:53:56 -0800

    added 404

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$('document').ready(function() {
+  var options = { 
+    videoId: 'e2t1axKELFA', 
+    start: 3,
+    mute: true
+  };
+  
+	$('#wrapper').tubular(options);
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+   // cdrHWbBQ-Nc Oil Pump
+   // rnQ7nayIcLo sun sparcles over the river
+});
